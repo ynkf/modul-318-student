424f7ca1fe6cb1f6c32b41b300b54109d54e569c
Simon Baumeler <simon.baumelerhotmail.com>
2018-12-02 12:16:53 +0100

    ui

---
+++ SwissTransportUI/DelegateCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace SwissTransportUI
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action _action;
+        private readonly Predicate<bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public DelegateCommand(Action action)
+        {
+            _action = action;
+        }
+
+        public DelegateCommand(Action action, Predicate<bool> canExecute)
+        {
+            _action = action;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(true);
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ SwissTransportUI/ViewModels/TransportOverviewViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using SwissTransport;
+
+namespace SwissTransportUI.ViewModels
+{
+    public class TransportOverviewViewModel
+    {
+        private string _startStation;
+        private string _destinationStation;
+        //private DateTime _dateAndTime;todo
+        //private bool _isDateAndTimeArrivalForArrival;
+
+        public TransportOverviewViewModel()
+        {
+            SearchCommand = new DelegateCommand(GetConnections, s => StartStation != null &&
+                                                                     DestinationStation != null);
+        }        
+
+        public DelegateCommand SearchCommand { get; }
+
+        public string StartStation
+        {
+            get =>_startStation;
+            set
+            {
+                _startStation = value;
+
+                SearchCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public string DestinationStation
+        {
+            get => _destinationStation;
+            set
+            {
+                _destinationStation = value;
+
+                SearchCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public DateTime DateAndTime { get; set; }
+
+        public bool IsDateAndTimeArrivalForArrival { get; set; }
+      
+
+        public Connections Connections { get; set; }
+
+        private void GetConnections()
+        {
+            var transport = new Transport();
+
+            //var connections = transport.GetConnections(SearchInfos.StartStation, SearchInfos.DestinationStation);
+            var connections = transport.GetConnections(_startStation, _destinationStation);
+
+            if (IsDateAndTimeArrivalForArrival)//todo
+            {
+                //connections.ConnectionList.Where(c => c.To.ArrivalTimestamp <= SearchInfos.Time);
+                connections.ConnectionList.Where(c => c.To.Arrival != "");
+            }
+            else
+            {
+                //connections.ConnectionList.Where(c => c.From.DepartureTimestamp <= SearchInfos.Time);
+                connections.ConnectionList.Where(c => c.From.Departure != "");
+            }
+        }
+    }
+}
